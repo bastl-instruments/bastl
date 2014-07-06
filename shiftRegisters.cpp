d0795d375bafd152e49db1c98bce13b2f3bc624f
LnnrtS <Lennart.Schierling@gmx.de>
2014-07-06 22:02:01 +0200

    add convenient class for sending data to shift registers

---
+++ shiftRegisters.cpp
@@ -0,0 +1,74 @@
+/*
+ * shiftRegisters.cpp
+ *
+ *  Created on: 06.07.2014
+ *      Author: user
+ */
+
+#include <shiftRegisters.h>
+#include <portManipulations.h>
+#include <Arduino.h>
+
+
+
+shiftRegisterBasic::shiftRegisterBasic() {
+	//should not be called
+}
+
+void shiftRegisterBasic::setup(
+		 volatile uint8_t* srck_port,
+		 volatile uint8_t* srck_ddr,
+		          uint8_t  srck_num,
+		 volatile uint8_t* rck_port,
+		 volatile uint8_t* rck_ddr,
+		 	 	  uint8_t  rck_num,
+		 volatile uint8_t* ser_port,
+		 volatile uint8_t* ser_ddr,
+		 	 	  uint8_t  ser_num) {
+
+	//save pointers to ports
+	this->srck_port = srck_port;
+	this->srck_num = srck_num;
+	this->rck_port = rck_port;
+	this->rck_num = rck_num;
+	this->ser_port = ser_port;
+	this->ser_num = ser_num;
+
+
+	//set clock pins as output
+	setHigh(*srck_ddr,srck_num);
+	setHigh(*rck_ddr,rck_num);
+
+	//set data pin mode
+	setHigh(*ser_ddr,ser_num);
+
+
+	//initialize clocks to low
+	setLow(*rck_port,rck_num);
+	setLow(*srck_port,srck_num);
+
+}
+
+void shiftRegisterBasic::write(uint8_t byte) {
+
+	cli();
+
+	uint8_t dataHigh = *ser_port | (1<<ser_num);
+	uint8_t dataLow = dataHigh & ~(1<<ser_num);
+
+	for (uint8_t index=0; index<8; index++) {
+
+		if ((byte >> index) & 1) *ser_port = dataHigh;
+		else			         *ser_port = dataLow;
+
+		//clock it in. only set bits to not influence SER port
+		uint8_t clkHigh = *srck_port | (1<<srck_num);
+		*srck_port = clkHigh;
+		*srck_port = clkHigh & ~(1<<srck_num);
+
+	}
+
+	sei();
+}
+
+
---
+++ shiftRegisters.h
@@ -0,0 +1,77 @@
+/*
+ * shiftRegisters.h
+ *
+ *  Created on: 06.07.2014
+ *      Author: user
+ */
+
+#include <portManipulations.h>
+#include <inttypes.h>
+
+#ifndef SHIFTREGISTERS_H_
+#define SHIFTREGISTERS_H_
+
+
+
+// Class to write data to 595 type shift registers
+//
+// after creating an instance, use setup() to set the pins you are using. three pins are need for operation:
+//   - SER:  data input to the shift register
+//   - SRCK: clock for taking the value from SER to the first storage register and shifting the other one step ahead
+//   - RCK:  enable the output of the shift register
+//
+// if you have several shift registers daisy chained, you can use write() multiple times
+
+class shiftRegisterBasic {
+public:
+
+	// empty. do not use!
+	shiftRegisterBasic();
+
+	// set up the pins you want to use
+	void setup(
+		 volatile uint8_t* srck_port,
+		 volatile uint8_t* srck_ddr,
+		          uint8_t  srck_num,
+		 volatile uint8_t* rck_port,
+		 volatile uint8_t* rck_ddr,
+		 	 	  uint8_t  rck_num,
+		 volatile uint8_t* ser_port,
+		 volatile uint8_t* ser_ddr,
+		 	 	  uint8_t  ser_num);
+
+	// write a byte to the shift register
+	// you have to enable the output manually
+	void write(uint8_t byte);
+
+	// enable output
+	inline void latch() {
+		setHigh(*rck_port,rck_num);
+		setLow(*rck_port,rck_num);
+	}
+
+	// clock in a single bit
+	inline void serClkIn() {
+		setHigh(*srck_port,srck_num);
+		setLow(*srck_port,srck_num);
+	}
+
+	// convenient method for setting the value of SER pin
+	inline void setDataBit(bool value) {
+		if (value) setHigh(*ser_port,ser_num);
+		else 	   setLow(*ser_port,ser_num);
+	}
+
+
+private:
+	volatile uint8_t* srck_port;  // the port SRCK pin is on
+	uint8_t			  srck_num;   // the number of the SRCK pin inside the SRCK port
+	volatile uint8_t* rck_port;   // ..same here
+	uint8_t			  rck_num;    // ...
+	volatile uint8_t* ser_port;
+	uint8_t           ser_num;
+};
+
+
+#endif /* SHIFTREGISTERS_H_ */
+
