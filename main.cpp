1a818c565dcfa95796ab23d60e6c661b9fe7ddfb
LnnrtS <Lennart.Schierling@gmx.de>
2014-07-11 11:57:10 +0200

    empty project

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+#include <main.h>
+
+
+
+
+int main(void) {
+
+  init();
+  setup();
+
+  while(true) {
+    loop();
+  }
+}
+
+
+
+
+
+void setup() {
+
+
+}
+
+
+void loop() {
+
+
+}
+
+
+
---
+++ main.h
@@ -0,0 +1,18 @@
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#include <Arduino.h>
+#include <portManipulations.h>
+
+
+void setup();
+void loop();
+
+
+
+
+#endif /* MAIN_H_ */
