b020eae3d9264c5b87f5da11e99859cd2ab1961c
Martin Baar <martin.baar@gmail.com>
2014-07-11 17:09:52 +0200

    Bastl library - Extend HW layer to be able to implement SRAM functionality

--- hw/IHWLayer.h
+++ hw/IHWLayer.h
@@ -1,11 +1,20 @@
 #ifndef IHWLAYER_H
 #define IHWLAYER_H
 
+#define BUTTON_DRUM_1 16
+#define BUTTON_DRUM_2 17
+#define BUTTON_DRUM_3 18
+
 class IHWLayer {
 public:
     virtual bool buttonDown(unsigned char index) = 0;
-    virtual void setDiod(unsigned char diod) = 0;
+    virtual void setDiod(unsigned char index) = 0;
     virtual unsigned char getKnobValue(unsigned char index) = 0;
+    virtual unsigned char readSRAM(long address) = 0;
+    virtual void readSRAMMore(long address, unsigned char * data, unsigned char size) = 0;
+    virtual void writeSRAM(long address, unsigned char data) = 0;
+    virtual void writeSRAMMore(long address, unsigned char * data, unsigned char size) = 0;
+
 };
 
 #endif // IHWLAYER_H
