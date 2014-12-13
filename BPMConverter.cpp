d49ab3c8e0e08ba4958b82dd413b35a4d9dddf2f
Václav Peloušek <info@bastl-instruments.com>
2014-12-13 17:07:52 +0100

    hovadinaFix

--- BPMConverter.cpp
+++ BPMConverter.cpp
@@ -8,7 +8,7 @@
 #include "BPMConverter.h"
 
 uint16_t BPMConverter::bpmToTimeUnits(uint16_t bpm, uint16_t timeUnitsPerSecond) {
-	return ((timeUnitsPerSecond * 60) / bpm)/32;
+	return ((timeUnitsPerSecond * 60) / bpm)/16;
 }
 
 uint16_t BPMConverter::timeUnitsToBPM(uint16_t timeUnitsPerBeat, uint16_t timeUnitsPerSecond) {
