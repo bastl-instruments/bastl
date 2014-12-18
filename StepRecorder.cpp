956a30eb6e9b948ec65d7fed88c0a7bf3263e9ca
Martin Baar <martin.baar@gmail.com>
2014-12-19 00:12:41 +0100

    SEKM-19 - Implement support for quantization in recorder

--- StepRecorder.cpp
+++ StepRecorder.cpp
@@ -7,19 +7,36 @@
 
 #include "StepRecorder.h"
 
-StepRecorder::StepRecorder() : player_(0), memory_(0), playerSettings_(0), record_(true) {
+StepRecorder::StepRecorder() : player_(0), memory_(0), playerSettings_(0), stepper_(0), record_(true) {
 }
 
-void StepRecorder::init(Player* player, IStepMemory * memory, PlayerSettings* playerSettings) {
+void StepRecorder::init(Player* player, IStepMemory * memory, PlayerSettings* playerSettings, BastlStepper * stepper) {
 	player_ = player;
 	memory_ = memory;
 	playerSettings_ = playerSettings;
+	stepper_ = stepper;
 }
 
 
 void StepRecorder::recordNote(unsigned char instrumentIndex) {
 	unsigned char currentStep = player_->getCurrentInstrumentStep(instrumentIndex);
 	unsigned char currentSubStep = player_->getCurrentInstrumentSubStep(instrumentIndex);
+
+	//Start of quantization evaluation
+	unsigned char quntizationSetting = (unsigned char)(playerSettings_->getRecordQuantizationType());
+	unsigned char quantizationRate = 1;
+	for (unsigned char powNumber = 0; powNumber < quntizationSetting; powNumber++) {
+		quantizationRate *= 2;
+	}
+
+	int doubleSubstepValue = (currentStep * 4 + currentSubStep) * 2;
+	doubleSubstepValue += stepper_->isCloserToNextStep() ? quantizationRate : 0;
+	doubleSubstepValue = (doubleSubstepValue / (quantizationRate * 2)) * quantizationRate;
+
+	currentStep = (unsigned char)(doubleSubstepValue / 4);
+	currentSubStep = (unsigned char)(doubleSubstepValue % 4);
+	//End of quantization evaluation
+
 	DrumStep step = memory_->getDrumStep(instrumentIndex, currentStep);
 
 	if (step.isMuted() || step.getSubStep(currentSubStep) == DrumStep::OFF) {
--- StepRecorder.h
+++ StepRecorder.h
@@ -11,18 +11,21 @@
 #include "Player.h"
 #include "PlayerSettings.h"
 #include "IStepMemory.h"
+#include <BastlStepper.h>
 
 class StepRecorder {
 public:
 	StepRecorder();
-	void init(Player * player, IStepMemory * memory, PlayerSettings * playerSettings);
+	void init(Player * player, IStepMemory * memory, PlayerSettings * playerSettings, BastlStepper * stepper);
 	void record(bool record);
 	void recordNote(unsigned char instrumentIndex);
 	void recordMIDINote(unsigned char channel, unsigned char note);
+	void setCurrentStepper(BastlStepper * stepper);
 private:
 	Player * player_;
 	IStepMemory * memory_;
 	PlayerSettings * playerSettings_;
+	BastlStepper * stepper_;
 
 	bool record_;
 };
@@ -30,6 +33,10 @@ private:
 inline void StepRecorder::record(bool record) {
 
 	record_ = record;
+}
+
+inline void StepRecorder::setCurrentStepper(BastlStepper * stepper) {
+	stepper_ = stepper;
 }
 
 
