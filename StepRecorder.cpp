720595349842eaed61c00b8a4df9aa7831db8baf
Martin Baar <martin.baar@gmail.com>
2014-12-30 07:46:02 +0100

    SEKM-23 Implement support for GATE instrument mode in Recorder

--- StepRecorder.cpp
+++ StepRecorder.cpp
@@ -8,6 +8,9 @@
 #include "StepRecorder.h"
 
 StepRecorder::StepRecorder() : player_(0), memory_(0), playerSettings_(0), stepper_(0), record_(true) {
+	for (unsigned char instrument = 0; instrument < 6; instrument++) {
+		recordInstrumentStatuses_[instrument] = false;
+	}
 }
 
 void StepRecorder::init(Player* player, IStepMemory * memory, PlayerSettings* playerSettings, BastlStepper * stepper) {
@@ -17,13 +20,12 @@ void StepRecorder::init(Player* player, IStepMemory * memory, PlayerSettings* pl
 	stepper_ = stepper;
 }
 
-
-void StepRecorder::recordNote(unsigned char instrumentIndex) {
+unsigned char StepRecorder::getCurrentQuantizedSubstep(unsigned char instrumentIndex) {
 	unsigned char currentStep = player_->getCurrentInstrumentStep(instrumentIndex);
 	unsigned char currentSubStep = player_->getCurrentInstrumentSubStep(instrumentIndex);
 
 	//Start of quantization evaluation
-	unsigned char quntizationSetting = (unsigned char)(playerSettings_->getRecordQuantizationType());
+	unsigned char quntizationSetting = (unsigned char) (playerSettings_->getRecordQuantizationType());
 	unsigned char quantizationRate = 1;
 	for (unsigned char powNumber = 0; powNumber < quntizationSetting; powNumber++) {
 		quantizationRate *= 2;
@@ -32,22 +34,52 @@ void StepRecorder::recordNote(unsigned char instrumentIndex) {
 	int doubleSubstepValue = (currentStep * 4 + currentSubStep) * 2;
 	doubleSubstepValue += stepper_->isCloserToNextStep() ? quantizationRate : 0;
 	doubleSubstepValue = (doubleSubstepValue / (quantizationRate * 2)) * quantizationRate;
+	return (unsigned char) doubleSubstepValue;
+}
 
-	currentStep = (unsigned char)(doubleSubstepValue / 4);
-	currentSubStep = (unsigned char)(doubleSubstepValue % 4);
-	//End of quantization evaluation
+void StepRecorder::startRecordNote(unsigned char instrumentIndex) {
 
-	DrumStep step = memory_->getDrumStep(instrumentIndex, currentStep);
+	recordInstrumentStatuses_[instrumentIndex] = true;
+	if (playerSettings_->getDrumInstrumentEventType(instrumentIndex) == PlayerSettings::TRIGGER) {
+		stopRecordNote(instrumentIndex);
+	}
+	player_->playNote(instrumentIndex, DrumStep::NORMAL);
+}
 
-	if (step.isMuted() || step.getSubStep(currentSubStep) == DrumStep::OFF) {
-		if (step.isMuted()) {
-			step.setMuted(false);
-			for (unsigned char subStep = 0; subStep < 4; subStep++) {
-				step.setSubStep(subStep, DrumStep::OFF);
+void StepRecorder::update() {
+
+	for (unsigned char instrument = 0; instrument < 6; instrument++) {
+		if (playerSettings_->getDrumInstrumentEventType(instrument) == PlayerSettings::GATE) {
+			if (recordInstrumentStatuses_[instrument]) {
+				recordSubstep(instrument, getCurrentQuantizedSubstep(instrument));
+				player_->playNote(instrument, DrumStep::NORMAL);
 			}
 		}
-		step.setSubStep(currentSubStep, DrumStep::NORMAL);
-		memory_->setDrumStep(instrumentIndex, currentStep, step);
+	}
+}
+
+void StepRecorder::recordSubstep(unsigned char instrumentIndex, unsigned char subStepIndex) {
+
+	unsigned char currentStep = (unsigned char)(subStepIndex / 4);
+	unsigned char currentSubStep = (unsigned char)(subStepIndex % 4);
+	//End of quantization evaluation
+	DrumStep manipulatedDrumStep = memory_->getDrumStep(instrumentIndex, currentStep);
+	if (manipulatedDrumStep.isMuted()) {
+		manipulatedDrumStep.setMuted(false);
+		for (unsigned char subStep = 0; subStep < 4; subStep++) {
+			manipulatedDrumStep.setSubStep(subStep, DrumStep::OFF);
+		}
+	}
+	manipulatedDrumStep.setSubStep(currentSubStep, DrumStep::NORMAL);
+	memory_->setDrumStep(instrumentIndex, currentStep, manipulatedDrumStep);
+
+}
+
+void StepRecorder::stopRecordNote(unsigned char instrumentIndex) {
+
+	if (recordInstrumentStatuses_[instrumentIndex]) {
+		recordInstrumentStatuses_[instrumentIndex] = false;
+		recordSubstep(instrumentIndex, getCurrentQuantizedSubstep(instrumentIndex));
 		player_->playNote(instrumentIndex, DrumStep::NORMAL);
 	}
 }
@@ -56,7 +88,8 @@ void StepRecorder::recordMIDINote(unsigned char channel, unsigned char note) {
 	unsigned char instrumentID = 0;
 	bool instrumentMapped = playerSettings_->getDrumInstrumentIndexFromMIDIMessage(channel, note, instrumentID);
 	if (instrumentMapped) {
-		recordNote(instrumentID);
+		startRecordNote(instrumentID);
+		stopRecordNote(instrumentID);
 	}
 }
 
