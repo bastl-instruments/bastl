396a9e12397ef26977992510390d35ffcdc7cef8
Martin Baar <martin.baar@gmail.com>
2015-01-07 21:26:48 +0100

    Hack HW interface to be able to use bastl cycles in the function view easy way

--- ILEDsAndButtonsHW.h
+++ ILEDsAndButtonsHW.h
@@ -6,7 +6,8 @@
 #include "IButtonHW.h"
 
 class ILEDsAndButtonsHW : public ILEDHW, public IButtonHW {
-
+public:
+	virtual unsigned int getElapsedBastlCycles() = 0;
 };
 
 #endif // ILEDSANDBUTTONSHW_H
