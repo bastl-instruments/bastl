ecbed97dbaee95e61142c23321273b29a397784a
Lennart <Lennart@bastl-instruments.com>
2015-01-30 18:21:23 +0100

    Rewrote LFO. Partly done

--- lfoExtended.h
+++ lfoExtended.h
@@ -14,248 +14,76 @@
 #define abs(x) ((x)>0?(x):-(x))
 
 
-
-#include "fixedMath.h"
 #include "random.h"
 
-typedef Q8n8 phaseType;
-const uint16_t numbSteps = 256;
-const uint8_t numbFBits = 8;
+#ifdef TESTING
+#include <stdio.h>
+#endif
+
+
 
 enum LFOBasicWaveform {SAW,TRIANGLE,RANDOM};
 enum LFOThresholdType {OVERFLOW,FOLDING};
 
-template <unsigned int UPDATEFREQ>
+
+
+
+
 class lfoExtended {
 
 public:
 	void init();
 
 public:
-	void setFreq(phaseType freq);
+	void setBastlCyclesPerPeriod(uint16_t bastlCyclesPerPeriod);
+	void setToStep(uint8_t stepNumber,uint8_t timestamp);
+
 	void setWaveform(LFOBasicWaveform waveform, bool invert = false, bool flop = false, LFOThresholdType type = FOLDING);
-	void setPhase(uint8_t position);
-	void setToStart();
 	void setResolution(uint8_t numbStepsToSkip);
 	void setThreshold(uint8_t thres);
-	void setCurrentValue(uint8_t);
+
+
 
 public:
-	LFOBasicWaveform getWaveform();
-	uint8_t getCurrentValue();
-	void step();
-
-	void dumpSettings();
+	uint8_t getValue(uint16_t timestamp);
 
 
 private:
-	void incrementPhase();
-	//void calcCurrentValue();
 
+	// Waveform
 	LFOBasicWaveform currentWaveform;
 	bool invertWaveform;
 	bool flopWaveform;
-
-	phaseType currentPhase;
-	uint8_t currentValue;
-
 	uint8_t threshold;
 	LFOThresholdType thresholdType;
 
-	phaseType phaseIncrement;
+	uint8_t currentStep;
+	uint8_t lastStep;
+
+	uint16_t timestampOffset;
+	uint16_t bastlCyclesPerPeriod;
+
+	uint8_t currentOutput;
 
 	uint8_t numbStepsToSkip;
 	uint8_t lastUnskippedStep;
+
+	// parameters
+	static const uint8_t maxAbsoluteSlope = 3;
+	static const uint8_t maxStepsBetweenSlopeChange = 30;
+	static const uint8_t minStepsBetweenSlopeChange = maxStepsBetweenSlopeChange/2;
+
+	static const uint8_t flopBit = 3;
+
+	// random
+	int8_t currentSlope;
+	int16_t stepsUntilNextSlopePick;
+
 };
 
 
 
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::init() {
-	currentWaveform = SAW;
-	currentValue = 0;
-	numbStepsToSkip = 0;
-	threshold = 255;
-	currentPhase = 0;
-	lastUnskippedStep=0;
 
-	setFreq(0);
-}
-
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setFreq(phaseType freq) {
-	phaseIncrement = (((uint32_t)freq*numbSteps))/UPDATEFREQ;
-	//Serial.println(phaseIncrement);
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setWaveform(LFOBasicWaveform waveform, bool invert, bool flop,LFOThresholdType type) {
-	currentWaveform = waveform;
-	invertWaveform = invert;
-	flopWaveform = flop;
-	thresholdType = type;
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setPhase(uint8_t pos) {
-	currentPhase = pos<<numbFBits;
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setToStart() {
-	setPhase(0);
-	currentValue();
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setResolution(uint8_t numbStepsToSkip) {
-	this->numbStepsToSkip = numbStepsToSkip;
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setThreshold(uint8_t thres) {
-	threshold = thres;
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::setCurrentValue(uint8_t value) {
-	currentValue = value;
-}
-
-template <unsigned int UPDATEFREQ>
-LFOBasicWaveform lfoExtended<UPDATEFREQ>::getWaveform() {
-	return currentWaveform;
-}
-
-template <unsigned int UPDATEFREQ>
-uint8_t lfoExtended<UPDATEFREQ>::getCurrentValue() {
-
-	const uint8_t flopBit = 3;
-
-
-
-
-	// calculate step from phase
-	uint8_t currentIndex = currentPhase>>numbFBits;
-
-	if ((uint8_t)(currentIndex-lastUnskippedStep) < numbStepsToSkip) {
-		currentIndex = lastUnskippedStep>>numbFBits;
-		//Serial.print("skipped "); Serial.println(currentIndex);
-	} else {
-		lastUnskippedStep = currentIndex;
-	}
-
-
-	// Render Basic Waveform
-	switch(currentWaveform) {
-		case SAW: {
-			currentValue = currentIndex;
-			break;
-		}
-		case TRIANGLE: {
-			if (currentIndex < numbSteps/2) currentValue = currentIndex*2;
-			else 				            currentValue = 255-(currentIndex-numbSteps/2)*2;
-			break;
-		}
-
-		case RANDOM: {
-
-			static int8_t currentSlope = 0;
-			static uint8_t currentTime = 5;
-
-			uint8_t maxStepSize = 2;
-			uint8_t maxTime = 100;
-			uint8_t minTime = maxTime/4;
-
-
-			if (currentTime != 0) {
-				currentTime--;
-			} else {
-				currentSlope = rand((char)-maxStepSize,(char)maxStepSize);
-				currentTime = rand(minTime,maxTime);
-			}
-
-
-			if (thresholdType == FOLDING) {
-				int16_t tmpCurrentValue = currentValue+currentSlope;
-				currentValue += currentSlope;
-				if (tmpCurrentValue > 255) {
-					currentValue = 255-currentValue;
-					currentSlope = -currentSlope;
-				}
-				if (tmpCurrentValue < 0) {
-					currentValue = -currentValue;
-					currentSlope = -currentSlope;
-				}
-			} else {
-				currentValue += currentSlope;
-			}
-
-
-
-			break;
-		}
-
-		default: break;
-	}
-
-
-	// Invert Waveform
-	if (invertWaveform) {
-		currentValue = 255-currentValue;
-	}
-
-	// Apply Flopping
-	if ((flopWaveform) && (currentValue & (1<<flopBit))) {
-		currentValue = 0;
-	}
-
-	// Apply Overflowing
-	if (currentValue > threshold) {
-
-		if (thresholdType == OVERFLOW) currentValue = currentValue % threshold;
-		if (thresholdType == FOLDING)  currentValue = threshold - (currentValue % threshold);
-	}
-
-	return currentValue;
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::step() {
-	incrementPhase();
-	//calcCurrentValue();
-}
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::incrementPhase() {
-	currentPhase += phaseIncrement;
-}
-
-
-
-template <unsigned int UPDATEFREQ>
-void lfoExtended<UPDATEFREQ>::dumpSettings() {
-
-	// Use printf here
-	/*
-
-	Serial.print("Basic Waveform: ");
-	switch (currentWaveform) {
-		case SAW: Serial.println("SAW"); break;
-		case TRIANGLE: Serial.println("TRAINGLE"); break;
-		case RANDOM: Serial.println("RANDOM"); break;
-	}
-
-
-	Serial.print("Invert: "); Serial.println(invertWaveform);
-	Serial.print("Flop: "); Serial.println(flopWaveform);
-	Serial.print("Thres: "); Serial.println(threshold);
-	Serial.print("Thres type: "); Serial.println(thresholdType);
-	Serial.print("Steps to skip: "); Serial.println(numbStepsToSkip);
-	Serial.print("Phase Inc: "); Serial.println(phaseIncrement);
-	*/
-}
 
 
 
--- test/LFO-Test.cpp
+++ test/LFO-Test.cpp
@@ -2,21 +2,42 @@
 #include <inttypes.h>
 #include "../lfoExtended.h"
 
-lfoExtended<1000> lfo;
+const uint8_t numbObjects = 4;
+
+float frequencies[numbObjects] = {0.1,1,3,5};
+uint16_t bastlCyclesPerSecond = 300;
+
+lfoExtended LFOs[numbObjects];
+
+
 
 
 
 int main( int argc, const char* argv[] ) {
 
-	lfo.init();
-	lfo.setWaveform(RANDOM);
-	lfo.setFreq(100<<4);
+	for (uint8_t index=0; index<numbObjects; index++) {
+		LFOs[index].init();
+		uint16_t bastlCyclesPerPeriod = (float)bastlCyclesPerSecond/(frequencies[index]);
+		LFOs[index].setBastlCyclesPerPeriod(bastlCyclesPerPeriod);
+		LFOs[index].setWaveform(RANDOM,false,false);
+		LFOs[index].setToStep(0,40);
+		LFOs[index].setResolution(0);
+		LFOs[index].setThreshold(100);
+	}
 
 
-	for (uint16_t index=0; index < 300; index++) {
-		lfo.step();
-		//lfo.getCurrentValue();
-		printf("%u\n",lfo.getCurrentValue());
+	uint32_t timeStamp = 0;
+
+	for (uint16_t index=0; index < 200; index++) {
+		timeStamp += 1;
+
+		printf("%u ",timeStamp);
+
+		for (uint8_t index=0; index<numbObjects; index++) {
+			printf("%u ",LFOs[index].getValue(timeStamp));
+		}
+
+		printf("\n");
 	}
 
 
