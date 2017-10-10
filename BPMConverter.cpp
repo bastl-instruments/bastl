68d49dc99561f8bf0ed6dc21a679e97d2b7fd86a
Martin Baar <martin.baar@gmail.com>
2017-10-11 01:17:34 +0200

    Implement precision BPM converter

--- BPMConverter.cpp
+++ BPMConverter.cpp
@@ -7,8 +7,13 @@
 
 #include "BPMConverter.h"
 
-uint16_t BPMConverter::bpmToTimeUnits(uint16_t bpm, uint16_t timeUnitsPerSecond) {
-	return ((timeUnitsPerSecond * 60) / bpm) / 16;
+uint16_t BPMConverter::bpmToTimeUnits(uint16_t bpm, uint16_t timeUnitsPerSecond, uint16_t sourceLeftOvers, uint16_t & leftOvers) {
+	uint16_t base = (timeUnitsPerSecond * 60) + ((sourceLeftOvers * 60) / 1000);
+	uint16_t division = (bpm * 16);
+	uint32_t returnValue = base / division;
+	uint32_t rest = (((uint32_t)base % (uint32_t)division) * 1000UL);
+	leftOvers = (uint16_t)(rest / (uint32_t)division);
+	return returnValue;
 }
 
 uint16_t BPMConverter::timeUnitsToBPM(uint16_t timeUnitsPerBeat, uint16_t timeUnitsPerSecond) {
--- BPMConverter.h
+++ BPMConverter.h
@@ -12,7 +12,7 @@
 
 class BPMConverter {
 public:
-	static uint16_t bpmToTimeUnits(uint16_t bpm, uint16_t timeUnitsPerSecond);
+	static uint16_t bpmToTimeUnits(uint16_t bpm, uint16_t timeUnitsPerSecond, uint16_t sourceLeftOvers, uint16_t & leftOvers);
 	static uint16_t timeUnitsToBPM(uint16_t timeUnits, uint16_t timeUnitsPerSecond);
 };
 
